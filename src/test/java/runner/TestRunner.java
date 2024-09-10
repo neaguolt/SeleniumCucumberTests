4ad9285d1be9009016c5fe2b225f9ae6ac1db068
leonidaa <leonidaa@gmail.com>
2024-09-10 20:49:03 +0200

    working code for first step

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,14 @@
 package runner;
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features= "src/test/java/features",
-        glue={"src/test/java/StepDefinitions","src/test/java/utility"},
-        plugin={"pretty","html:target/cucumber-html-report","json:cucumber.json"}
+        features = "src/test/java/features",
+        glue = {"src/test/java/StepDefinitions", "src/test/java/utility"},
+        plugin = {"pretty", "html:target/cucumber-html-report", "json:cucumber.json", "junit:target/cucumber-junit-report.xml"}
+
 )
 public class TestRunner {
-
-
 }
